42142b88eca395d3d11051a7014622e14409af84
yutadd <spyk4m49@gmail.com>
2023-11-20 21:57:53 +0900

    [fix]GPT was wrong

--- addresscalc.py
+++ addresscalc.py
@@ -1,12 +1,18 @@
 def get_network_bits(*ips):
     bins = [''.join(format(int(x), '08b') for x in ip.split('.')) for ip in ips]
     minlen = min(len(b) for b in bins)
-    network_bits = sum(all(b[i] == bins[0][i] for b in bins) for i in range(minlen))
-    return network_bits
+    networkbits=0
+    for i in range(minlen):
+        model_bit=bins[0][i]
+        for _bin in bins:
+            if model_bit!=_bin[i]:
+                return networkbits
+        networkbits+=1
+    return networkbits
 
 ips = []
 while True:
-    ip = input("アドレスを入力してください（終了する場合は空白を入力）:")
+    ip = input("アドレスを入力してください（終了する場合は空白を入力）:").strip()
     if ip:
         ips.append(ip)
     else:
