0d27269c0e1a03a584beab7df5a97960257758fb
yutadd <spyk4m49@gmail.com>
2023-11-20 21:44:37 +0900

    [update] GPT fixed some bugs

--- addresscalc.py
+++ addresscalc.py
@@ -1,21 +1,15 @@
-one=input("一つ目のアドレス:")
-two=input("二つ目のアドレス:")
-splited_one=one.split(".")
-splited_two=two.split(".")
-bin_one= [format(int(x),'b') for x in splited_one]
-bin_two=[format(int(x),'b') for x in splited_two]
-stacked_one=""
-stacked_two=""
-for o in bin_one:
-    stacked_one+=o
-for o in bin_two:
-    stacked_two+=o
-minlen=min([len(stacked_two),len(stacked_one)])
-network_bits=0
+def get_network_bits(*ips):
+    bins = [''.join(format(int(x), '08b') for x in ip.split('.')) for ip in ips]
+    minlen = min(len(b) for b in bins)
+    network_bits = sum(all(b[i] == bins[0][i] for b in bins) for i in range(minlen))
+    return network_bits
 
-for i in range(minlen):
-    if(stacked_one[i]!=stacked_two[i]):
-        break
+ips = []
+while True:
+    ip = input("アドレスを入力してください（終了する場合は空白を入力）:")
+    if ip:
+        ips.append(ip)
     else:
-        network_bits+=1
-print(network_bits)
+        break
+
+print(get_network_bits(*ips))
